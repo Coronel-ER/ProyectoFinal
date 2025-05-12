3a5476dbf69a51d50bb83c53e6d027c156db89a4
Coronel-ER <emilianocr@cetys.edu.mx>
2025-05-12 16:44:45 -0700

    Update dron.hpp

--- dron.hpp
+++ dron.hpp
@@ -1,1 +1,19 @@
+#ifndef DRON_HPP
+#define DRON_HPP
 
+#include <SFML/Graphics.hpp>
+
+class Dron {
+private:
+    sf::Sprite sprite;
+    sf::Texture textura;
+    sf::Vector2f velocidad;
+
+public:
+    Dron();
+    void mover();
+    void dibujar(sf::RenderWindow& ventana);
+    const sf::Sprite& getSprite() const;
+};
+
+#endif
