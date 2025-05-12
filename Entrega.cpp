1405163e69ab057558e9c15f0804696be7da50c4
Alexys8532 <alexys.luna@cetys.edu.mx>
2025-05-12 12:05:46 -0700

    Update Entrega.cpp

--- Entrega.cpp
+++ Entrega.cpp
@@ -11,15 +11,29 @@ protected:
 	vector<int> posicion;
 	string localizacion;
 	bool entregado;
-	Usuario puntuacion;
 public:
 	Entrega(vector<int> posicion, string localizacion, bool entregado) : posicion(posicion), localizacion(localizacion), entregado(entregado){}
 
 	virtual void Entregas() {
 		if (entregado == true) {
-			cout << "La entrega ha sido completada exitosamente \n";
+			cout << "La entrega ha sido completada exitosamente, +1000 puntos \n";
 			puntuacion = puntuacion + 1000;
 		}
 	}
 	virtual ~Entrega() {}
+};
+
+class EntregasEspeciales:: public Entrega {
+
+
+public:
+	EntregasEspeciales(vector<int> posicion, string localizacion, bool entregado) : Entrega(posicion, localizacion, entregado) {}
+
+	void Entregas() override {
+		if (entregado == true) {
+			cout << "La entrega especial ha sido completada exitosamente +2000 puntos \n";
+			puntuacion = puntuacion + 2000;
+		}
+	}
+
 };
