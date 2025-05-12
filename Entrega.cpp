63587846bc344187305f7c93da555a85607f61ea
Alexys8532 <alexys.luna@cetys.edu.mx>
2025-05-12 11:38:59 -0700

    Create Entrega.cpp

---
+++ Entrega.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include "juego.hpp"
+#include <string>
+
+using namespace std;
+
+
+class Entrega {
+protected:
+	vector<int> posicion;
+	string localizacion;
+public:
+	Entrega(vector<int> posicion, string localizacion){}
+
+
+};
