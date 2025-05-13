664f14f1e00558f8a7e90e8dfc3ca991b6a552f5
Coronel-ER <emilianocr@cetys.edu.mx>
2025-05-12 18:26:41 -0700

    Update FuncionalidadDronConSfml.cpp

--- FuncionalidadDronConSfml.cpp
+++ FuncionalidadDronConSfml.cpp
@@ -3,15 +3,15 @@
 int main() {
     sf::RenderWindow window(sf::VideoMode(800, 600), "Dron sobre campo verde");
     window.setFramerateLimit(60);
-
+// Estoy cargando la textura del dron desde un archivo (no salio)
     sf::Texture droneTexture;
     if (!droneTexture.loadFromFile("dron.png")) {
-        return -1;
+        return -1;// Si no se puede cargar, sale con error
     }
 
     sf::Sprite droneSprite;
     droneSprite.setTexture(droneTexture);
-    droneSprite.setScale(0.3f, 0.3f);
+    droneSprite.setScale(0.3f, 0.3f);// esta creo que la voy a hacer un poco mas grande dependieno los avances de mis compañeros
     droneSprite.setPosition(400, 300);
 
     const float speed = 5.0f;
@@ -27,7 +27,7 @@ int main() {
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) droneSprite.move(0, speed);
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) droneSprite.move(-speed, 0);
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) droneSprite.move(speed, 0);
-
+        //pintar el fonde de verde 
         window.clear(sf::Color(102, 204, 102));
         window.draw(droneSprite);
         window.display();
