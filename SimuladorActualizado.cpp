d81cc05a0b22f7f5df05fab4f7402ce4d4fef64b
Coronel-ER <emilianocr@cetys.edu.mx>
2025-05-18 13:52:51 -0700

    Create SimuladorActualizado.cpp

---
+++ SimuladorActualizado.cpp
@@ -0,0 +1,64 @@
+//Agregue setnivel y el ejecutar para que sirva con el menu
+//tambien elimine lugares para que mejor por nivel
+#include "Juego.hpp"
+#include "Colisiones.hpp"
+#include <iostream>
+
+Juego::Juego() : ventana(sf::VideoMode(800, 600), "CETYSWings - Nivel"), nivelActual(1) {
+    ventana.setFramerateLimit(60);
+}
+
+void Juego::setNivel(int nivel) {
+    nivelActual = nivel;
+    entregas.clear();
+
+    if (nivel == 1) {
+        entregas.push_back(new Entrega(sf::Vector2f(300, 100), "Zona Río"));
+        entregas.push_back(new Entrega(sf::Vector2f(500, 200), "Playas"));
+        entregas.push_back(new Entrega(sf::Vector2f(400, 300), "Zona Centro"));
+        entregas.push_back(new Entrega(sf::Vector2f(200, 400), "La Presa"));
+    } else if (nivel == 2) {
+        entregas.push_back(new EntregaEspecial(sf::Vector2f(600, 300), "Otay", 10));
+        entregas.push_back(new Entrega(sf::Vector2f(400, 250), "La Mesa"));
+        entregas.push_back(new EntregaEspecial(sf::Vector2f(150, 350), "El Florido", 15));
+        entregas.push_back(new Entrega(sf::Vector2f(300, 450), "5 y 10"));
+    } else if (nivel == 3) {
+        entregas.push_back(new EntregaEspecial(sf::Vector2f(150, 500), "CETYS Universidad", 20));
+        entregas.push_back(new Entrega(sf::Vector2f(100, 100), "Zona Centro"));
+        entregas.push_back(new EntregaEspecial(sf::Vector2f(600, 150), "Chapultepec", 25));
+        entregas.push_back(new Entrega(sf::Vector2f(250, 350), "Club Campestre"));
+    }
+}
+
+void Juego::ejecutar() {
+    while (ventana.isOpen()) {
+        procesarEventos();
+        actualizar();
+        renderizar();
+    }
+}
+
+void Juego::procesarEventos() {
+    sf::Event evento;
+    while (ventana.pollEvent(evento)) {
+        if (evento.type == sf::Event::Closed)
+            ventana.close();
+    }
+    dron.mover();
+}
+
+void Juego::actualizar() {
+    for (auto& e : entregas) {
+        if (!e->estaEntregada() && Colisiones::detectar(dron.getSprite(), e->posicion)) {
+            e->entregar();
+        }
+    }
+}
+
+void Juego::renderizar() {
+    ventana.clear(sf::Color(102, 204, 102));
+    for (auto& e : entregas)
+        e->dibujar(ventana);
+    dron.dibujar(ventana);
+    ventana.display();
+}
