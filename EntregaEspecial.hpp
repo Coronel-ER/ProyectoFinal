b192e3c0bd6110a01238d3087278b6e2fa158989
Alexys8532 <alexys.luna@cetys.edu.mx>
2025-05-21 19:21:21 -0700

    Update EntregaEspecial.hpp

--- EntregaEspecial.hpp
+++ EntregaEspecial.hpp
@@ -1,1 +1,26 @@
 
+
+#include <iostream>
+#include <vector>
+#include "juego.hpp"
+#include <string>
+
+using namespace std;
+
+class EntregasEspeciales:: public Entrega {
+
+
+public:
+	EntregasEspeciales(vector<int> posicion, string localizacion, bool entregado) : Entrega(posicion, localizacion, entregado) {}
+
+
+  
+	void Entregas() override {
+		if (entregado == true) {
+			cout << "La entrega especial ha sido completada exitosamente +2000 puntos \n";
+			subirPuntuacion(2000);
+		}
+	}
+  ~EntregaEspeciales(){}
+
+};
