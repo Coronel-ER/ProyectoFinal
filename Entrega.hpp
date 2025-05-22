66bef1a3b91b0568f6f936c222266a54dab96e7d
Alexys8532 <alexys.luna@cetys.edu.mx>
2025-05-21 19:18:08 -0700

    Update Entrega.hpp

--- Entrega.hpp
+++ Entrega.hpp
@@ -1,1 +1,24 @@
+#include <iostream>
+#include <vector>
+#include "juego.hpp"
+#include <string>
 
+using namespace std;
+
+
+class Entrega {
+protected:
+	vector<int> posicion;
+	string localizacion;
+	bool entregado;
+public:
+	Entrega(vector<int> posicion, string localizacion, bool entregado) : posicion(posicion), localizacion(localizacion), entregado(entregado){}
+
+	virtual void Entregas() {
+		if (entregado == true) {
+			cout << "La entrega ha sido completada exitosamente, +1000 puntos \n";
+			subirPuntuacion(1000);
+		}
+	}
+	virtual ~Entrega() {cout << "Entrega ha sido eliminada \n" }
+};
