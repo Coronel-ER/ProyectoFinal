9ab4b806a398e02f9446dfe65b29010430272e4e
alejandroCJimenez <contreras.jose@cetys.edu.mx>
2025-05-22 01:13:01 -0700

    Update main.cpp
    Solucion de errores y mejora del apartado visual

--- main.cpp
+++ main.cpp
@@ -1,47 +1,83 @@
+#include <iostream>
 #include <SFML/Graphics.hpp>
 #include "Simulador.hpp"
-#include <iostream>
 
-int mostrarMenu(sf::RenderWindow& window, sf::Font& font) {
+
+// Pendiente Crear Clase Menu
+
+// Impresion del menu
+int mostrarMenu(sf::RenderWindow& window, sf::Font& font, sf::Font& TitleFont) {
+    // Arreglo de opciones
     std::vector<std::string> opciones = {
         "Iniciar Nivel 1",
         "Iniciar Nivel 2",
         "Iniciar Nivel 3",
         "Salir"
     };
-
+   
     std::vector<sf::Text> textos;
     int seleccion = 0;
 
+    // Impresion Titulo
+    sf::Text titulo(TitleFont);
+    titulo.setString("CETYSwings");
+    titulo.setCharacterSize(65);
+    titulo.setFillColor(sf::Color(20,225,20));
+    titulo.setPosition({ ((800.f - titulo.getLocalBounds().size.x) / 2), 80 });
+
+    // Impresion de las opciones
     for (size_t i = 0; i < opciones.size(); ++i) {
-        sf::Text texto(opciones[i], font, 30);
-        texto.setPosition(250, 200 + i * 50);
+        sf::Text texto(font);
+        texto.setString(opciones[i]);
+        texto.setCharacterSize(30);
+        texto.setPosition({ ((800.f - texto.getLocalBounds().size.x) / 2), 250.f + i * 50 });
         textos.push_back(texto);
     }
 
+    // Bucle principal
     while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed)
+        // Crear ventana
+        while (const std::optional event = window.pollEvent()) {
+            // Cerrar ventana
+            if (event -> is <sf::Event::Closed>()){
                 window.close();
-            if (event.type == sf::Event::KeyPressed) {
-                if (event.key.code == sf::Keyboard::Up)
-                    seleccion = (seleccion + opciones.size() - 1) % opciones.size();
-                if (event.key.code == sf::Keyboard::Down)
-                    seleccion = (seleccion + 1) % opciones.size();
-                if (event.key.code == sf::Keyboard::Enter)
-                    return seleccion + 1;
+            }
+            
+            // Manejo de opciones
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Up)) {
+                seleccion = (seleccion + opciones.size() - 1) % opciones.size();
+            }
+            else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Down)) {
+                seleccion = (seleccion + 1) % opciones.size();
+            }
+            else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Enter)) {
+                return seleccion + 1;
             }
         }
 
-        window.clear(sf::Color(100, 149, 237));
+        // ---------- Display ----------
+
+        // Fondo
+        sf::Texture texture("recursos/MenuBG.png");
+        sf::Sprite sprite(texture);
+
+        window.clear(sf::Color(0,0,0));
+        window.draw(sprite);
+
+        // Impresion del titulo
+        window.draw(titulo);
+
+        //Impresion del texto
         for (size_t i = 0; i < textos.size(); ++i) {
+            // Efecto Hover
             if (i == seleccion)
                 textos[i].setFillColor(sf::Color::Yellow);
             else
-                textos[i].setFillColor(sf::Color::White);
+                textos[i].setFillColor(sf::Color::Black);
             window.draw(textos[i]);
         }
+
+
         window.display();
     }
 
@@ -49,19 +85,34 @@ int mostrarMenu(sf::RenderWindow& window, sf::Font& font) {
 }
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "CETYSWings - Menu");
-    sf::Font font;
-    if (!font.loadFromFile("recursos/arial.ttf")) {
-        std::cerr << "Error al cargar la fuente
-";
+    // Creacion de la ventana principal
+    sf::RenderWindow window(sf::VideoMode({ 800, 600 }, 24), "CETYSWings - Menu");
+
+    // Inicializacion de la fuente del titulo
+    sf::Font TitleFont;
+    if (!TitleFont.openFromFile("recursos/PixelTitle.ttf")) { // Cambio de fuente tipografica
+        std::cerr << "Error al cargar la fuente del titulo";
         return -1;
     }
 
-    int seleccion = mostrarMenu(window, font);
+    // Inicializacion de la fuente 
+    sf::Font font;
+    if (!font.openFromFile("recursos/PixelFont.ttf")) { // Cambio de fuente tipografica
+        std::cerr << "Error al cargar la fuente";
+        return -1;
+    }
+
+    // Impresion del menu regresa la opcion seleccionada
+    int seleccion = mostrarMenu(window, font, TitleFont);
+
+    // Inicia el juego en su respectivo nivel
     if (seleccion >= 1 && seleccion <= 3) {
-        Simulador juego;
-        juego.setNivel(seleccion);
-        juego.ejecutar();
+        
+        /*
+        Simulador simulador;
+        simulador.setNivel(seleccion);
+        simulador.ejecutar();
+        */
     }
 
     return 0;
