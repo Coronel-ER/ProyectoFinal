7a49ed9842d8eb44b0c3fc5e3dfa7724f8ee521c
alejandroCJimenez <contreras.jose@cetys.edu.mx>
2025-05-22 01:40:49 -0700

    Main OOP.cpp
    Se paso el menu a clase y se agrego la libreria vector en caso de ser necesaria

--- main.cpp
+++ main.cpp
@@ -1,88 +1,100 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
+#include <vector>
 #include "Simulador.hpp"
 
 
-// Pendiente Crear Clase Menu
-
-// Impresion del menu
-int mostrarMenu(sf::RenderWindow& window, sf::Font& font, sf::Font& TitleFont) {
-    // Arreglo de opciones
-    std::vector<std::string> opciones = {
-        "Iniciar Nivel 1",
-        "Iniciar Nivel 2",
-        "Iniciar Nivel 3",
-        "Salir"
-    };
-   
+class Menu {
+private:
+    sf::RenderWindow& window;
+    sf::Font& font;
+    sf::Font& TitleFont;
+    std::vector<std::string> opciones;
     std::vector<sf::Text> textos;
-    int seleccion = 0;
+    int seleccion;
 
-    // Impresion Titulo
-    sf::Text titulo(TitleFont);
-    titulo.setString("CETYSwings");
-    titulo.setCharacterSize(65);
-    titulo.setFillColor(sf::Color(20,225,20));
-    titulo.setPosition({ ((800.f - titulo.getLocalBounds().size.x) / 2), 80 });
-
-    // Impresion de las opciones
-    for (size_t i = 0; i < opciones.size(); ++i) {
-        sf::Text texto(font);
-        texto.setString(opciones[i]);
-        texto.setCharacterSize(30);
-        texto.setPosition({ ((800.f - texto.getLocalBounds().size.x) / 2), 250.f + i * 50 });
-        textos.push_back(texto);
+public:
+    // ---------- Constructor ----------
+    Menu(sf::RenderWindow& window, sf::Font& font, sf::Font& TitleFont) : window(window), font(font), TitleFont(TitleFont), seleccion(0) {
+        opciones = {
+            "Iniciar Escenario 1",
+            "Iniciar Escenario 2",
+            "Iniciar Escenario 3",
+            "Salir"
+        };
     }
 
-    // Bucle principal
-    while (window.isOpen()) {
-        // Crear ventana
-        while (const std::optional event = window.pollEvent()) {
-            // Cerrar ventana
-            if (event -> is <sf::Event::Closed>()){
-                window.close();
-            }
-            
-            // Manejo de opciones
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Up)) {
-                seleccion = (seleccion + opciones.size() - 1) % opciones.size();
-            }
-            else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Down)) {
-                seleccion = (seleccion + 1) % opciones.size();
-            }
-            else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Enter)) {
-                return seleccion + 1;
-            }
+    // ---------- Funciones ----------
+
+    // Impresion del menu
+    int mostrarMenu() {
+        // Impresion Titulo
+        sf::Text titulo(TitleFont);
+        titulo.setString("CETYSwings");
+        titulo.setCharacterSize(65);
+        titulo.setFillColor(sf::Color(20, 225, 20));
+        titulo.setPosition({ ((800.f - titulo.getLocalBounds().size.x) / 2), 80 });
+
+        // Impresion de las opciones
+        for (size_t i = 0; i < opciones.size(); ++i) {
+            sf::Text texto(font);
+            texto.setString(opciones[i]);
+            texto.setCharacterSize(30);
+            texto.setPosition({ ((800.f - texto.getLocalBounds().size.x) / 2), 250.f + i * 50 });
+            textos.push_back(texto);
         }
 
-        // ---------- Display ----------
+        // Bucle principal
+        while (window.isOpen()) {
+            // Crear ventana
+            while (const std::optional event = window.pollEvent()) {
+                // Cerrar ventana
+                if (event->is <sf::Event::Closed>()) {
+                    window.close();
+                }
 
-        // Fondo
-        sf::Texture texture("recursos/MenuBG.png");
-        sf::Sprite sprite(texture);
+                // Manejo de opciones
+                if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Up)) {
+                    seleccion = (seleccion + opciones.size() - 1) % opciones.size();
+                }
+                else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Down)) {
+                    seleccion = (seleccion + 1) % opciones.size();
+                }
+                else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Enter)) {
+                    return seleccion + 1;
+                }
+            }
 
-        window.clear(sf::Color(0,0,0));
-        window.draw(sprite);
+            // ---------- Display ----------
 
-        // Impresion del titulo
-        window.draw(titulo);
+            // Fondo
+            sf::Texture texture("recursos/MenuBG.png");
+            sf::Sprite sprite(texture);
 
-        //Impresion del texto
-        for (size_t i = 0; i < textos.size(); ++i) {
-            // Efecto Hover
-            if (i == seleccion)
-                textos[i].setFillColor(sf::Color::Yellow);
-            else
-                textos[i].setFillColor(sf::Color::Black);
-            window.draw(textos[i]);
+            window.clear(sf::Color(0, 0, 0));
+            window.draw(sprite);
+
+            // Impresion del titulo
+            window.draw(titulo);
+
+            //Impresion del texto
+            for (size_t i = 0; i < textos.size(); ++i) {
+                // Efecto Hover
+                if (i == seleccion)
+                    textos[i].setFillColor(sf::Color::Green);
+                else
+                    textos[i].setFillColor(sf::Color::Black);
+                window.draw(textos[i]);
+            }
+
+
+            window.display();
         }
-
-
-        window.display();
+        return 4;
     }
 
-    return 4;
-}
+};
+
 
 int main() {
     // Creacion de la ventana principal
@@ -102,17 +114,29 @@ int main() {
         return -1;
     }
 
-    // Impresion del menu regresa la opcion seleccionada
-    int seleccion = mostrarMenu(window, font, TitleFont);
+    // Creacion del menu
+    Menu menu(window, font, TitleFont);
 
-    // Inicia el juego en su respectivo nivel
+    // Impresion del menu y captura de su resultado
+    int seleccion = menu.mostrarMenu();
+
+    // Inicia el juego en su respectivo Escenario
     if (seleccion >= 1 && seleccion <= 3) {
-        
         /*
         Simulador simulador;
-        simulador.setNivel(seleccion);
+        simulador.setEscenario(seleccion);
         simulador.ejecutar();
         */
+        if (seleccion == 1)
+        {
+            std::cerr << "Se ejecuto el escenario numero uno";
+        }
+        else if (seleccion == 2) {
+            std::cerr << "Se ejecuto el escenario numero dos";
+        }
+        else if (seleccion == 3) {
+            std::cerr << "Se ejecuto el escenario numero tres";
+        }
     }
 
     return 0;
