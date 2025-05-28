10cacff7c164c11835ed9ac53c6d3b51ec6eca2b
Coronel-ER <emilianocr@cetys.edu.mx>
2025-05-27 21:09:11 -0700

    Delete src directory

--- src/Colisiones.cpp
+++
@@ -1,7 +0,0 @@
-#include "Colisiones.hpp"
-
-namespace Colisiones {
-    bool detectar(const sf::Sprite& sprite1, const sf::Vector2f& punto) {
-        return sprite1.getGlobalBounds().contains(punto);
-    }
-}
